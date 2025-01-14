40b6d1674de88785d5893d59c5d74c4b49cc3c46
Ole-Edvard Ørebæk <ole.e.orebak@hiof.no>
2025-01-14 11:11:17 +0100

    Uploaded lecture 3 code

---
+++ Forelesninger/Forelesning3 - Metoder, Klasser og Objekter/Course.java
@@ -0,0 +1,8 @@
+// Klasse som modellerer en egendefinert datatype som representerer kurs
+public class Course {
+
+    // Instansvariabler som er felles for alle kurs. Representerer kurs sine "egenskaper".
+    public String courseName;
+    public String courseCode;
+
+}
