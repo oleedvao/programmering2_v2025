1f846383682d304de95f52c08461cc5628e2f9bd
Ole-Edvard Ørebæk <ole.e.orebak@hiof.no>
2025-01-20 10:01:11 +0100

    Uploaded pre lecture classes for lecture 5

---
+++ Forelesninger/Forelesning 5 - Objektreferanser og Objektlister/Før forelesning/Course.java
@@ -0,0 +1,31 @@
+package no.hiof.oleedvao.forelesning5pre.model;
+
+public class Course {
+    private String courseName;
+    private String courseCode;
+
+    public Course() {
+
+    }
+
+    public Course(String courseName, String courseCode) {
+        this.courseName = courseName;
+        this.courseCode = courseCode;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getCourseCode() {
+        return courseCode;
+    }
+
+    public void setCourseCode(String courseCode) {
+        this.courseCode = courseCode;
+    }
+}
---
+++ Forelesninger/Forelesning 5 - Objektreferanser og Objektlister/Før forelesning/Student.java
@@ -0,0 +1,65 @@
+package no.hiof.oleedvao.forelesning5pre.model;
+
+public class Student {
+
+    private String firstName;
+    private String lastName;
+    private int age;
+    private String studentId;
+    private String birthNumber = "12345678910";
+
+    public Student() {
+
+    }
+
+    public Student(String firstName, String lastName, int age, String studentId) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.studentId = studentId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (age < 0) {
+            this.age = 0;
+        }
+        else if (age > 120) {
+            this.age = 120;
+        }
+        else {
+            this.age = age;
+        }
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getBirthNumber() {
+        return birthNumber;
+    }
+}
