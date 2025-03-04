8321f33f2a7cbf327489ae161720ccf74b6b1fc6
Ole-Edvard Ørebæk <ole.e.orebak@hiof.no>
2025-03-04 08:42:18 +0100

    Uploaded the rest of lecture 15 code examples

---
+++ Forelesninger/Forelesning 15 - CSV-filer, Maven/Maven/Forelesning15Maven/src/main/java/no/hiof/oleedvao/forelesning15maven/Main.java
@@ -0,0 +1,15 @@
+package no.hiof.oleedvao.forelesning15maven;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class Main {
+    public static void main(String[] args) {
+
+        /*
+        Denne klassen kan bare importeres og benyttes dersom jackson-databind er blitt lagt til som en dependency
+        i pom.xml. Se pom.xml og slides for hvordan dette gjøres.
+         */
+        ObjectMapper objectMapper = new ObjectMapper();
+
+    }
+}
